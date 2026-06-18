7b4cd0c0623ef699607043c5edee280183e22ef9
giorgigio1 <giorgigiorgadze93@gmail.com>
2026-06-18 17:44:04 +0400

    including 80 lessons

---
+++ src/main/java/com/codewithmosh/store/services/Jwt.java
@@ -0,0 +1,34 @@
+package com.codewithmosh.store.services;
+
+import com.codewithmosh.store.entities.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class Jwt {
+    private final Claims claims;
+    private final SecretKey secretKEy;
+
+    public Jwt(Claims claims, SecretKey secretKEy) {
+        this.claims = claims;
+        this.secretKEy = secretKEy;
+    }
+
+    public boolean isExpired() {
+        return claims.getExpiration().before(new Date());
+    }
+
+    public Long getUserId() {
+        return Long.valueOf(claims.getSubject());
+    }
+
+    public Role getRole() {
+        return Role.valueOf(claims.get("role", String.class));
+    }
+
+    public String toString() {
+        return Jwts.builder().claims(claims).signWith(secretKEy).compact();
+    }
+}
